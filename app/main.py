767d445618dd4fd90e0aedc0bcd553c1c5560c43
Sergiy Kulanov <sergey@kulanov.org.ua>
2024-06-12 15:12:15 +0300

    Update app/main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from typing import Union
 
 from fastapi import FastAPI
+from .config import Settings
 
 app = FastAPI()
+settings = Settings()
 
 
 @app.get("/")
@@ -13,3 +15,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/config")
+def get_oidc_config():
+    return {
+        "issuer": settings.OIDC_ISSUER,
+        "client_id": settings.OIDC_CLIENT_ID,
+        "client_secret": settings.OIDC_CLIENT_SECRET
+    }
